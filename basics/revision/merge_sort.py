96e9fb870b1dd5d2418a7ed53a51804389d2d381
maifujalam <Newman12!@>
2022-03-15 12:42:36 +0530

    ubuntu-vm

---
+++ basics/revision/merge_sort.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.stdin = open("input.txt")
+a = [int(i) for i in input().strip().split()]
+
+
+def merge(a):
+    if len(a) > 1:
+        mid = len(a) // 2
+        L=a[:mid]
+        R=a[mid:]
+        merge(L)
+        merge(R)
+        i=j=k=0
+        while i<len(L) and j <len(R):
+            if L[i]<R[j]:
+                a[k]=L[i]
+                i+=1
+            else:
+                a[k]=R[j]
+                j+=1
+            k+=1
+        while i<len(L):
+            a[k]=L[i]
+            i+=1
+            k+=1
+        while j < len(R):
+            a[k]=R[j]
+            j+=1
+            k+=1
+
+merge(a)
+print(a)
