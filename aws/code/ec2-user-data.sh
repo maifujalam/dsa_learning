e6761c0cb9b65f1809cfe1a8f393a1f1bdea1e97
maifujalam <Newman12!@>
2022-05-11 20:42:46 +0530

    sort

--- aws/code/ec2-user-data.sh
+++ aws/code/ec2-user-data.sh
@@ -6,3 +6,10 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+
+## Ubuntu
+#      apt update
+#      apt -y install apache2
+#      cat <<EOF > /var/www/html/index.html
+#      <html><body><p>Linux startup script added directly.</p></body></html>'
