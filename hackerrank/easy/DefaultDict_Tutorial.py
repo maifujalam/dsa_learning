d795468ff2ea9fe1f34a8896b164e2aa8c8ed863
maifujalam <sk.maifujalam@gmail.com>
2022-06-20 10:14:15 +0530

    DefaultDict

--- hackerrank/easy/DefaultDict_Tutorial.py
+++ hackerrank/easy/DefaultDict_Tutorial.py
@@ -1,17 +1,27 @@
+from operator import ge
 import sys
 import collections
 
-sys.stdin = open('input.txt', 'r')
+sys.stdin = open("input.txt", 'r')
 
 
 def counters(A, B):
-    print(A,B)
-    collections.defaultdict
-    print(a)
+    ans=collections.defaultdict(list)
+    for i in range(len(A)):
+        ans[A[i]].append(i)
+    for j in B:
+        get_item=ans[j]
+        for k in get_item:
+            print(k+1,end=' ')
+        if get_item==[]:
+            print('-1',end=' ')
+        print()
+
 
 if __name__ == '__main__':
     n, m = map(int, input().strip().split())
-    A = B = []
+    A=[]
+    B=[]
     for i in range(n):
         A.append(input())
     for i in range(m):
